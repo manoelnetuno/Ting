0b9f31dd40bcfab51c7b057fc4d1d378fb24bf71
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-12-09 18:48:23 -0300

    refatorando word_seach

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,45 +1,71 @@
-
-def exists_word(word, queue):
-    results = []
+def find_occurrences_in_file(word, file):
+    occurrences = []
     word_lower = word.lower()
 
-    for index in range(len(queue)):
-        file = queue.search(index)
-        occurrences = []
     with open(file['nome_do_arquivo'], 'r', encoding='utf-8') as f:
         for line_number, line in enumerate(f, start=1):
             if word_lower in line.lower():
                 occurrences.append({'linha': line_number})
 
-            if occurrences:
-                results.append({
-                    'palavra': word,
-                    'arquivo': file['nome_do_arquivo'],
-                    'ocorrencias': occurrences
+    return occurrences
+
+
+def find_occurrences_with_content_in_file(word, file):
+    occurrences = []
+    word_lower = word.lower()
+
+    with open(file['nome_do_arquivo'], 'r', encoding='utf-8') as f:
+        for line_number, line in enumerate(f, start=1):
+            if word_lower in line.lower():
+                occurrences.append({
+                    'linha': line_number,
+                    'conteudo': line.strip()
                 })
 
-    return results
+    return occurrences
 
 
-def search_by_word(word, queue):
+def exists_word(word, queue):
     results = []
-    word_lower = word.lower()
+    processed_files = set()  #
 
     for index in range(len(queue)):
         file = queue.search(index)
-        occurrences = []
-        with open(file['nome_do_arquivo'], 'r', encoding='utf-8') as f:
-            for line_number, line in enumerate(f, start=1):
-                if word_lower in line.lower():
-                    occurrences.append({
-                        'linha': line_number,
-                        'conteudo': line.strip()})
+
+        if file['nome_do_arquivo'] in processed_files:
+            continue
+
+        occurrences = find_occurrences_in_file(word, file)
 
         if occurrences:
             results.append({
                 'palavra': word,
                 'arquivo': file['nome_do_arquivo'],
                 'ocorrencias': occurrences
-                })
+            })
+            processed_files.add(file['nome_do_arquivo'])
+
+    return results
+
+
+def search_by_word(word, queue):
+    results = []
+    processed_files = set()
+
+    for index in range(len(queue)):
+        file = queue.search(index)
+
+        if file['nome_do_arquivo'] in processed_files:
+            continue
+
+        occurrences = find_occurrences_with_content_in_file(word, file)
+
+        if occurrences:
+            results.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occurrences
+            })
+            processed_files.add(file['nome_do_arquivo'])
 
     return results
