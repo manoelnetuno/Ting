64ce473efcb55b84dbb729f39fd60bcbfad1e31e
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-12-09 18:22:55 -0300

    word_seach

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,4 @@
 
-
 def exists_word(word, queue):
     results = []
     word_lower = word.lower()
@@ -22,5 +21,25 @@ def exists_word(word, queue):
     return results
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word, queue):
+    results = []
+    word_lower = word.lower()
+
+    for index in range(len(queue)):
+        file = queue.search(index)
+        occurrences = []
+        with open(file['nome_do_arquivo'], 'r', encoding='utf-8') as f:
+            for line_number, line in enumerate(f, start=1):
+                if word_lower in line.lower():
+                    occurrences.append({
+                        'linha': line_number,
+                        'conteudo': line.strip()})
+
+        if occurrences:
+            results.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occurrences
+                })
+
+    return results
