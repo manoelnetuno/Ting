25ae3017205804f9190a515317fe7dd72d8cde48
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-12-12 14:36:29 -0300

    testes

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,36 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_line = PriorityQueue()
+
+    file1 = {'nome_do_arquivo': 'file1.txt', 'qtd_linhas': 3}
+    file2 = {'nome_do_arquivo': 'file2.txt', 'qtd_linhas': 7}
+    file3 = {'nome_do_arquivo': 'file3.txt', 'qtd_linhas': 1}
+
+    priority_line.enqueue(file3)
+    priority_line.enqueue(file2)
+    priority_line.enqueue(file1)
+
+    assert len(priority_line) == 3
+    assert len(priority_line.high_priority) == 2
+    assert len(priority_line.regular_priority) == 1
+
+    assert priority_line.high_priority.search(0) == file3
+    assert priority_line.high_priority.search(1) == file1
+    assert priority_line.regular_priority.search(0) == file2
+
+    assert priority_line.search(0) == file3
+    assert priority_line.search(1) == file1
+    assert priority_line.search(2) == file2
+
+    with pytest.raises(IndexError):
+        priority_line.search(5)
+
+    assert priority_line.dequeue() == file3
+    assert priority_line.dequeue() == file1
+    assert priority_line.dequeue() == file2
+
+    with pytest.raises(IndexError):
+        priority_line.dequeue()
